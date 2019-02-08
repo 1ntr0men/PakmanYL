48041202073f5149e904d5989a63af0887200ff5
Tsarev A. A <tsarevantuan@yandex.ru>
2019-02-08 20:20:04 +0300

    Added object classes

--- objects.py
+++ objects.py
@@ -1,10 +1,84 @@
-class Object:
-    pass
+import pygame
+import os
+
+
+pygame.init()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+class Object(pygame.sprite.Sprite):
+
+    def __init__(self, group, sheet, columns, rows, x, y):
+        super().__init__(group)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
 
 
 class PacMan(Object):
-    pass
+    def __init__(self, group, x, y):
+        super().__init__(group, load_image('pacman.png'), x, y, 5, 1)
 
 
 class Spirit(Object):
-    
+    pass
+
+
+class Shadow(Spirit):
+    pass
+
+
+class Speedy(Spirit):
+    pass
+
+
+class Bashful(Spirit):
+    pass
+
+
+class Pokey(Spirit):
+    pass
+
+
+size = w, h = 500, 300
+screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
+PacMan(all_sprites, 0, 0)
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    all_sprites.draw(screen)
+    all_sprites.update()
